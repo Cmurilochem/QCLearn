f996b14db8943b20a87683dcf6bb35006ac92cfb
Cmurilochem <carlosmurilorocha@gmail.com>
2023-01-10 16:52:03 +0100

    updating classes cpp

--- src/classes.cpp
+++ src/classes.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <fstream>   // open, read and write to an external file
+#include <stdlib.h>  // necessary to abort 
 #include "classes.h"
 
 Job::Job(){
@@ -18,12 +19,30 @@ void Job::getData(std::string filename){
 
     if (myfile.is_open()){
         
-        getline(myfile,temp);                    // reading the whole first line
+        getline(myfile,temp);                    // reading the job type (whole first line)
         type = temp.substr(temp.find(" ")+1,-1); // removing the "*JOB" part
 
-        
-        
+        //
+        /* handling exceptions - checking for errors */
+        //
+
+        try { 
+            if (type != "PARTICLE_BOX_1D") {
+                throw (type); // if the input is wrong, throw an error defined in catch 
+            }
+        }
+        catch (std::string mytype) { 
+            std::cout << "Error!" << std::endl;
+            std::cout << "Please, provide a valid job type" << std::endl;
+            std::cout << "Input job type ---> " << mytype << " <--- is not implemented" << std::endl;
+            exit(0);
+        }
+
+        getline(myfile,temp);                    // reading second entire line
+        std::cout << temp.substr(temp.find(" ")+1,-1) << std::endl; 
+
         myfile.close();
+
     } else {
         std::cout << "Unable to open input file" << std::endl;
     }
@@ -36,9 +55,10 @@ void Job::getData(std::string filename){
 void Job::putData(){
     
     if (type == "PARTICLE_BOX_1D") {
-        std::cout << "Particle in a 1D box selected" << std::endl;
+        std::cout << "Particle in a box selected" << std::endl;
     } else {
         std::cout << "ERROR" << std::endl;
+        exit(0);
     }
     
     

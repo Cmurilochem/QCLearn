21d0fdeac3c59bb6d68a2fd334f5a0a55980dd0a
Cmurilochem <carlosmurilorocha@gmail.com>
2023-01-24 15:01:57 +0100

    gitignore update

--- tests/testwfpartbox.cpp
+++ tests/testwfpartbox.cpp
@@ -1,24 +1,24 @@
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp> //<catch2/catch.hpp>
 #include "particle_box.h"
 #include "constants.h"
 
-TEST_CASE( "ChekingWaveFunction at middle", "") {
+TEST_CASE( "Cheking Wave Function at middle", "") {
     
     REQUIRE( particleBoxExactWF(1,2.0,1.0) == 1.0 );  
 }
 
-TEST_CASE( "ChekingWaveFunction at corner", "") {
+TEST_CASE( "Cheking Wave Function at corner", "") {
     
     REQUIRE( particleBoxExactWF(1,2.0,2.0) < 1.0e-12 );
 }
 
-TEST_CASE( "ChekingWaveFunction at origin", "") {
+TEST_CASE( "Cheking Wave Function at origin", "") {
     
     REQUIRE( particleBoxExactWF(1,2.0,0.0) < 1.0e-12 );
 }
 
-TEST_CASE( "ChekingWaveFunction excited state at middle", "") {
+TEST_CASE( "Cheking Wave Function: excited state at middle", "") {
     
     REQUIRE( particleBoxExactWF(2,2.0,1.0) < 1.0e-12 );  
 }
